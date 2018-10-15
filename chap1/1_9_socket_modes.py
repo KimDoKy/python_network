5ae175e73a39ca6cab289c78f4d345afbe5a443b
KimDoKy <makingfunk0@gmail.com>
2018-10-15 14:26:13 +0900

    add chap1_9

---
+++ chap1/1_9_socket_modes.py
@@ -0,0 +1,15 @@
+import socket
+
+def test_socket_modes():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setblocking(1)
+    s.settimeout(0.5)
+    s.bind(("127.0.0.1", 0))
+
+    socket_address = s.getsockname()
+    print("Trivial Server lauched on socket: %s" % str(socket_address))
+    while 1:
+        s.listen(1)
+
+if __name__ == "__main__":
+    test_socket_modes()
